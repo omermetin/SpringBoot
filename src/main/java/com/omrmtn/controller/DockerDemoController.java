7f4f819c192cecbe4f70b1c1b7762b119eab16e5
omermetin <omermetin@hotmail.co.uk>
2020-02-01 00:35:28 +0000

    changed

--- src/main/java/com/omrmtn/controller/DockerDemoController.java
+++ src/main/java/com/omrmtn/controller/DockerDemoController.java
@@ -1,4 +1,4 @@
-package com.mimaraslan.demo.controller;
+package com.omrmtn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class DockerDemoController {
 
-	// http://localhost:8085/test
+    // http://localhost:8085/test
     @GetMapping("test")
-    public String testDemo(){
+    public String testDemo() {
         return "LOLO 8085 Docker App Running....!";
     }
 }
--- src/main/java/com/omrmtn/controller/UserController.java
+++ src/main/java/com/omrmtn/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.mimaraslan.demo.controller;
+package com.omrmtn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
